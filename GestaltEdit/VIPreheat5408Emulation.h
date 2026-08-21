faf2d980e970cb544d7ed9aa13c5555f6eaea1b5
Yugu Li <liyugu971208@gmail.com>
2026-08-21 18:15:06 +0800

    diag: expose beta5 preheat bundle variants

--- GestaltEdit/VIPreheat5408Emulation.h
+++ GestaltEdit/VIPreheat5408Emulation.h
@@ -3,5 +3,7 @@
 NS_ASSUME_NONNULL_BEGIN
 
 FOUNDATION_EXPORT NSString *VIPreheat5408EmulationGenerateReport(void);
+FOUNDATION_EXPORT NSString *VIPreheat5408EmulationGenerateCameraReport(void);
+FOUNDATION_EXPORT NSString *VIPreheat5408EmulationGenerateVisualIntelligenceCameraReport(void);
 
 NS_ASSUME_NONNULL_END
